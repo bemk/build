d9a3918181660cc15b4af6429ceb74ee50eb59e2
Bart Kuivenhoven <bemkuivenhoven@gmail.com>
2014-10-14 11:40:55 +0200

    test: Add optional commandline params for test
    Signed-off-by: Bart Kuivenhoven <bemkuivenhoven@gmail.com>

--- test.sh
+++ test.sh
@@ -6,36 +6,36 @@ PROJECT=test
 
 function clean()
 {
-	$BUILD_DIR/build -c --config $PROJECT.config $PROJECT.build
+	$BUILD_DIR/build -c --config $PROJECT.config $PROJECT.build $FLAGS
 }
 
 function test_no()
 {
 	echo "Compiling test project with all flags NO"
-	$BUILD_DIR/build --config $PROJECT.config $PROJECT.build --configure --allno-config
-	$BUILD_DIR/build --config $PROJECT.config $PROJECT.build -t random
+	$BUILD_DIR/build --config $PROJECT.config $PROJECT.build --configure --allno-config $FLAGS
+	$BUILD_DIR/build --config $PROJECT.config $PROJECT.build -t random $FLAGS
 }
 
 function test_yes()
 {
 	echo "Compiling test project with all flags YES"
-	$BUILD_DIR/build --config $PROJECT.config $PROJECT.build --configure --allyes-config
-	$BUILD_DIR/build --config $PROJECT.config $PROJECT.build -t random 
+	$BUILD_DIR/build --config $PROJECT.config $PROJECT.build --configure --allyes-config $FLAGS
+	$BUILD_DIR/build --config $PROJECT.config $PROJECT.build -t random $FLAGS
 }
 
 function test_random()
 {
 	echo "Compiling test project with random flags"
-	$BUILD_DIR/build --config $PROJECT.config $PROJECT.build --random-config
-	$BUILD_DIR/build --config $PROJECT.config $PROJECT.build 
+	$BUILD_DIR/build --config $PROJECT.config $PROJECT.build --random-config $FLAGS
+	$BUILD_DIR/build --config $PROJECT.config $PROJECT.build $FLAGS
 	
 }
 
 function test_user()
 {
 	echo "Compiling test project with user defined flags"
-	$BUILD_DIR/build --config $PROJECT.config $PROJECT.build --configure
-	$BUILD_DIR/build --config $PROJECT.config $PROJECT.build -t random
+	$BUILD_DIR/build --config $PROJECT.config $PROJECT.build --configure $FLAGS
+	$BUILD_DIR/build --config $PROJECT.config $PROJECT.build -t random $FLAGS
 }
 
 echo "Testing build..."
